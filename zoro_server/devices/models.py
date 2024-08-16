5e04afeb2a47ea6ce7217ddb9345922a182772c5
tsoihim <skfk7081@gmail.com>
2024-08-16 23:58:12 +0900

    Add NetDevice Model

--- zoro_server/devices/models.py
+++ zoro_server/devices/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-class SomeDevice(models.Model):
-    name = models.CharField(max_length=32)
-    ip = models.CharField(max_length=15)
-    priority = models.IntegerField(default=0)
-    last_update = models.IntegerField(default=0)
+class NetDevice(models.Model):
+    serial = models.CharField(unique=True, max_length=32)
+    ip = models.CharField(null=False, max_length=15)
+    priority = models.IntegerField(default=10)
+    last_update = models.DateTimeField("date updated")
+    
+    def __str__(self):
+        return self.serial
