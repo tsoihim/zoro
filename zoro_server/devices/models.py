d8b587e8764981b423e3505a31c9759e6f76c72d
tsoihim <skfk7081@gmail.com>
2024-08-16 20:16:01 +0900

    Defining a model

--- zoro_server/devices/models.py
+++ zoro_server/devices/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class SomeDevice(models.Model):
+    name = models.CharField(max_length=32)
+    ip = models.CharField(max_length=15)
+    priority = models.IntegerField(default=0)
+    last_update = models.IntegerField(default=0)
