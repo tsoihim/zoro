e225dea836f0fc07cb1d26aa73995d02c2e5e988
tsoihim <skfk7081@gmail.com>
2024-08-27 09:36:27 +0900

    Apply audo_now

--- zoro_server/devices/models.py
+++ zoro_server/devices/models.py
@@ -4,7 +4,7 @@ class NetDevice(models.Model):
     serial = models.CharField(unique=True, max_length=32)
     ip = models.CharField(null=False, max_length=15)
     priority = models.IntegerField(default=10)
-    last_update = models.DateTimeField("date updated")
-    
+    last_update = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.serial
