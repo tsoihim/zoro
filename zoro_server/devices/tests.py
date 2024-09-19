d94e98ee5d5dad5a733213414ce7b7e84e4d5080
tsoihim <skfk7081@gmail.com>
2024-09-19 16:48:52 +0900

    Add simple test

--- zoro_server/devices/tests.py
+++ zoro_server/devices/tests.py
@@ -1,3 +1,15 @@
+from django.db import IntegrityError
 from django.test import TestCase
+from devices.models import NetDevice
 
-# Create your tests here.
+class NetDeviceTestCase(TestCase):
+    def setUp(self):
+        NetDevice.objects.create(serial="A123", ip="12.1.1.1")
+        NetDevice.objects.create(serial="A124", ip="12.1.1.1")
+
+    def insert_dup(self):
+        NetDevice.objects.create(serial="A124", ip="12.1.1.1")
+
+    def test_net_device_dump(self):
+        print(NetDevice.objects.all().values())
+        self.assertRaises(IntegrityError, self.insert_dup)
